08cbe545d8d0fba32a574d245acf245b023cd2c1
Toby Law <toby@tzcl.me>
2021-03-19 08:46:25 +1100

    Implemented commandline parsing

--- allocate.c
+++ allocate.c
@@ -1,7 +1,37 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-  printf("Hello, world!");
+  bool custom_scheduler = false;
+  int num_processors = 0;
+  char *filename = NULL;
+
+  int opt;
+  while ((opt = getopt(argc, argv, "f:p:c")) != -1) {
+    switch (opt) {
+    case 'f':
+      filename = optarg;
+      break;
+    case 'p':
+      num_processors = atoi(optarg);
+      break;
+    case 'c':
+      custom_scheduler = true;
+      break;
+    case '?':
+      fprintf(stderr, "Usage: %s -%c requires an argument!\n", argv[0], optopt);
+      exit(EXIT_FAILURE);
+    default:
+      fprintf(stderr, "Usage: %s [-fpc]\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  printf("filename: %s, num_processors: %d, flag c: %s\n", filename,
+         num_processors, custom_scheduler ? "true" : "false");
 
   return 0;
 }
